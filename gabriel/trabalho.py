3f8f07948e1ca457408d85df234ad9e3a87b6582
Gabriel 2000 <166823901+gbr12122000@users.noreply.github.com>
2024-05-18 22:30:59 -0300

    Add files via upload

---
+++ gabriel/trabalho.py
@@ -0,0 +1,56 @@
+import cmath
+import math
+
+print("Operações disponíveis:")
+print("1. Soma")
+print("2. Subtração")
+print("3. Multiplicação")
+print("4. Divisão")
+print("5. Raiz Quadrada")
+print("6. Seno")
+print("7. Cosseno")
+print("8. Tangente")
+
+opcao = input("Escolha uma operação (1/2/3/4/5/6/7/8): ")
+
+if opcao == '1':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 + num2
+    print(f"Resultado: {resultado}")
+elif opcao == '2':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 - num2
+    print(f"Resultado: {resultado}")
+elif opcao == '3':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 * num2
+    print(f"Resultado: {resultado}")
+elif opcao == '4':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    if num2 == 0:
+        print("Erro: Divisão por zero!")
+    else:
+        resultado = num1 / num2
+        print(f"Resultado: {resultado}")
+elif opcao == '5':
+    num = float(input("Digite o número para calcular a raiz quadrada: "))
+    resultado = cmath.sqrt(num)  # Usando cmath.sqrt para raiz quadrada de números negativos
+    print(f"Resultado: {resultado}")
+elif opcao == '6':
+    angulo = float(input("Digite o ângulo em graus: "))
+    resultado = math.sin(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Seno: {resultado:.2f}")
+elif opcao == '7':
+    angulo = float(input("Digite o ângulo em graus: "))
+    resultado = math.cos(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Cosseno: {resultado:.2f}")
+elif opcao == '8':
+    angulo = float(input("Digite o ângulo em graus: "))
+    resultado = math.tan(math.radians(angulo))  # Convertendo o ângulo de graus para radianos
+    print(f"Tangente:{resultado:.2f}")
+else:
+    print("Opção inválida!")
